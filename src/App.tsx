0712fc3a32a283d6f6f108e1aee817a2ccd21ccb
davka1230 <davka1230m@gmail.com>
2026-03-02 01:22:27 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter="/vanguard-strategic-display">
         <Navbar />
         <Routes>
           <Route path="/" element={<Index />} />
